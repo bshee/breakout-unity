afa1a3b93de8f873ac79af295e3de8d4514bb761
Brian Shee <bri6294an@gmail.com>
2014-03-26 21:42:19 -0700

    Initial project setup

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+  public float speed;
+  
+  private Vector3 velocity;
+  
+	// Use this for initialization
+	void Start () {
+    velocity = new Vector3(0, speed * Mathf.Sin(-90), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.position += velocity * Time.deltaTime;
+	}
+  
+  void OnCollisionEnter(Collision collision) {
+    Debug.Log("Ouch!");
+  }
+}
---
+++ Assets/Scripts/BatControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatControl : MonoBehaviour {
+
+  [Range(0, 4)]
+  public float speed;
+  
+  private int width;
+  //private int height = 16;
+  private float direction;
+  
+  private Vector3 minBounds;
+  private Vector3 maxBounds;
+  
+	// Use this for initialization
+	void Start() {
+    width = 32;
+    minBounds = Camera.main.ScreenToWorldPoint(new Vector3(width/2, 0, 10));
+    maxBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - width/2, Screen.height, 10));
+    Debug.Log(minBounds.ToString());
+    //Debug.Log("Camera pixel height " + Camera.main.pixelHeight);
+    //Debug.Log("Camera pixel width " + Camera.main.pixelWidth);
+    //Debug.Log(Camera.main.WorldToScreenPoint(transform.position).ToString()) ;
+    //Vector3 pos = new Vector3(Screen.width / 2, 10, 10);
+    //transform.position = Camera.main.ScreenToWorldPoint(pos);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+    Vector3 currentPos = transform.position;
+    float dir = Input.GetAxis("Horizontal");
+    
+    // need to bound by x coordinates
+    float newX = currentPos.x + dir * speed * Time.deltaTime;
+    
+    if (newX < minBounds.x) {
+      newX = minBounds.x;      
+    }
+    else if (newX > maxBounds.x) {
+      newX = maxBounds.x;
+    }
+    
+    transform.position = new Vector3(newX, currentPos.y, 0);
+	}
+  
+  // fun with physics
+  void FixedUpdate() {
+    
+  }
+}
