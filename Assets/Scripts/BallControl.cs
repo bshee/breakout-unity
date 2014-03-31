ba07124bd9b3da8152f1e664c20a1ea578a8e4ab
Brian Shee <bshee@ucdavis.edu>
2014-03-31 09:13:41 -0700

    Added zone to reset ball, removed some depracated code from Bat

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+  public float speed;
+  
+  private Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+    rigidbody2D.velocity = Vector3.down * speed;
+    startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    //transform.position += velocity * Time.deltaTime;
+	}
+  
+  void FixedUpdate() {
+  }
+  
+  void OnCollisionEnter2D(Collision2D collision) {
+    if (collision.collider.tag == "Lose") {
+      Reset();
+    }
+    else {
+      rigidbody2D.velocity = speed * Vector3.Reflect(collision.relativeVelocity, collision.contacts[0].normal).normalized;
+      //Debug.Log(rigidbody2D.velocity);
+      
+      if (rigidbody2D.velocity.y == 0) {
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Random.value * 2 - 1);
+      }
+    }
+  }
+  
+  void Reset() {
+    transform.position = startPosition;
+    rigidbody2D.velocity = Vector2.up * -speed;
+  }
+}
--- Assets/Scripts/BatControl.cs
+++ Assets/Scripts/BatControl.cs
@@ -6,24 +6,8 @@ public class BatControl : MonoBehaviour {
   [Range(0, 4)]
   public float speed;
   
-  private int width;
-  //private int height = 16;
-  private float direction;
-  
-  private Vector3 minBounds;
-  private Vector3 maxBounds;
-  
 	// Use this for initialization
 	void Start() {
-    width = 32;
-    minBounds = Camera.main.ScreenToWorldPoint(new Vector3(width/2, 0, 10));
-    maxBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - width/2, Screen.height, 10));
-    
-    //Debug.Log("Camera pixel height " + Camera.main.pixelHeight);
-    //Debug.Log("Camera pixel width " + Camera.main.pixelWidth);
-    //Debug.Log(Camera.main.WorldToScreenPoint(transform.position).ToString()) ;
-    //Vector3 pos = new Vector3(Screen.width / 2, 10, 10);
-    //transform.position = Camera.main.ScreenToWorldPoint(pos);
 	}
 	
 	// Update is called once per frame
