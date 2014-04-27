a15af1d0ef7582e3330a1b64b26e52fb89c3fe19
Brian Shee <bri6294an@gmail.com>
2014-04-26 21:35:10 -0700

    Added breakable blocks and score control
      Added Prefabs folder for organization

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breakable : MonoBehaviour {
+  
+  public GameObject player;
+  public int points;
+  
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+  
+  void OnCollisionEnter2D(Collision2D collision) {
+  
+    player.GetComponent<ScoreControl>().UpdateScore(points);
+    Destroy(gameObject);
+  }
+}
---
+++ Assets/Scripts/ScoreControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreControl : MonoBehaviour {
+  
+  private int score;
+  
+	// Use this for initialization
+	void Start () {
+    score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+  
+  public void UpdateScore(int points) {
+    score += points;
+    Debug.Log(score);
+  }
+}
