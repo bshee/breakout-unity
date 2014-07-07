8d871f88809229011b25cbd9e8d9b9d4ecbeab5f
Brian Shee <bri6294an@gmail.com>
2014-07-07 13:50:51 -0700

    Added title scene
    Added title font
    Organized with Fonts folders and Materials.
    First test build and other data.

---
+++ Assets/Scripts/PromptText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// flashes text and also goes to next scene when spacebar is pressed
+public class PromptText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      Application.LoadLevel("Level1");
+    }
+	}
+}
