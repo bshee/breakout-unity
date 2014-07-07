1bafe33ea7e58057fc176420c087e04d319af97c
Brian Shee <bri6294an@gmail.com>
2014-07-07 15:46:31 -0700

    Made prompt text fade out and in over 1 second.

--- Assets/Scripts/PromptText.cs
+++ Assets/Scripts/PromptText.cs
@@ -3,16 +3,46 @@ using System.Collections;
 
 // flashes text and also goes to next scene when spacebar is pressed
 public class PromptText : MonoBehaviour {
+  
+  private float fadeDuration = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+    StartCoroutine(FadeEffect());
 	}
-	
+  
+  IEnumerator FadeEffect() {
+    bool fadeOut = true;
+    //bool fadeIn = false;
+    
+    while (true) {
+      Color c = guiText.material.color;
+      
+      if (fadeOut) {
+        c.a -= Time.deltaTime/fadeDuration;
+        
+        if (c.a <= 0) {
+          fadeOut = false;
+        }
+      }
+      else { // fade in
+        c.a += Time.deltaTime/fadeDuration;
+        if (c.a >= 1) {
+          fadeOut = true;
+        }
+      }
+      
+      guiText.material.color = c;
+      
+      yield return null;
+      //yield return new WaitForSeconds(0.05f);
+    }
+  }
+
 	// Update is called once per frame
 	void Update () {
     if (Input.GetKeyDown(KeyCode.Space)) {
       Application.LoadLevel("Level1");
-    }
+    }   
 	}
 }
